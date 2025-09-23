61fc6086562b4b8a4156e5a90ce1b4e8d651111c
Sakshi Bhagwat <bhagwat.sakshi864@gmail.com>
2025-09-23 21:19:16 +0530

    shift mongo to online server

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define MongoDB connection URL
-//const mongoURL = 'mongodb://localhost:27017/hotels'
-const mongoURL = 'mongodb+srv://sakshi:sakshi08@cluster0.ulpp8yg.mongodb.net/'
+//const mongoURL = 'process.env.MONGODB_URL_LOCAL'
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL,{
